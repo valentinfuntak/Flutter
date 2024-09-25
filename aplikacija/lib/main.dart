8facc82e9982642413a787677879e9915ca0aaad
valentinfuntak <valentinfuntakk@gmail.com>
2024-09-25 11:56:37 +0200

    Forma

--- aplikacija/lib/main.dart
+++ aplikacija/lib/main.dart
@@ -9,7 +9,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Obrazac(),
@@ -33,14 +33,49 @@ class Gumb extends StatelessWidget {
 }
 
 class Obrazac extends StatelessWidget {
-  const Obrazac({super.key});
+  Obrazac({super.key});
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController surnameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Unesite svoje ime i prezime",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(
+                labelText: 'Ime', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          TextField(
+            controller: surnameController,
+            decoration: const InputDecoration(
+                labelText: 'Prezime', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                String ime = nameController.text;
+                String prezime = surnameController.text;
+                debugPrint("Ime $ime, Prezime: $prezime");
+              },
+              child: Text('Potvrdi'))
+        ],
       ),
     );
   }
