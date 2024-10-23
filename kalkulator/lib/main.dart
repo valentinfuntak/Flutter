18b2288681f7ab0e3bfc321a33e7ea8417d839b1
valentinfuntak <valentinfuntakk@gmail.com>
2024-10-23 11:46:39 +0200

    1. kalkulator

--- kalkulator/lib/main.dart
+++ kalkulator/lib/main.dart
@@ -9,12 +9,51 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text('Kalkulator postotaka'),
         ),
+        body: KalkulatorPostotakaForm(),
       ),
     );
+  }
+}
+
+class KalkulatorPostotakaForm extends StatefulWidget {
+  @override
+  _KalkulatorPostotakaFormState createState() =>
+      _KalkulatorPostotakaFormState();
+}
+
+class _KalkulatorPostotakaFormState extends State<KalkulatorPostotakaForm> {
+  final TextEditingController postotakController = TextEditingController();
+  final TextEditingController ukupnoController = TextEditingController();
+  final TextEditingController dioController = TextEditingController();
+  final TextEditingController pocetnaVrijednostController =
+      TextEditingController();
+  final TextEditingController krajnjaVrijednostController =
+      TextEditingController();
+
+  double? rezultatPostotka;
+  double? postotakDijela;
+  double? promjenaPodataka;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(children: [
+        buildRow(context, "Koliko je X% od Y?",
+        postotakController,
+        ukupnoController,"Izračunaj", (){
+          setState(() {
+            double postotak = double.parse(postotakController.text);
+            double total = double.parse(ukupnoController.text);
+            rezultatPostotka = (postotak / 100) * ukupno;
+          });
+        })
+      ],),
+    )
   }
 }
